2cb015ba365155b0f68fc484df87c881b894ae0a
PlayerMarci <marton@szabomarci.hu>
2021-10-07 11:31:39 +0200

    feltöltöttem

---
+++ sargaturo.js
@@ -0,0 +1,11 @@
+function adag(){
+    var i = getElementById("fo").value;
+    document.getElementById("tej").innerText = i / 5;
+    document.getElementById("tojas").innerText = i * 2;
+    document.getElementById("").innerText = i * 1
+
+    
+
+    
+
+}
